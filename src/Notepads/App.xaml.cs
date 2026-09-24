2451fe1658280ab8dc210ab71182fbb7b5373638
phwyverysad <woranat.fluke@gmail.com>
2026-09-25 00:17:31 +0700

    feat(i18n): add comprehensive Thai (th-TH) localization and set as default language

--- src/Notepads/App.xaml.cs
+++ src/Notepads/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿// ---------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------
 //  Copyright (c) 2019-2024, Jiaqi (0x7c13) Liu. All rights reserved.
 //  See LICENSE file in the project root for license information.
 // ---------------------------------------------------------------------------------------------
@@ -59,6 +59,11 @@ namespace Notepads
             LoggingService.LogInfo($"[{nameof(App)}] Started: Instance = {InstanceId} IsPrimaryInstance: {IsPrimaryInstance} IsGameBarWidget: {IsGameBarWidget}.");
 
             ApplicationSettingsStore.Write(SettingsKey.ActiveInstanceIdStr, App.InstanceId.ToString());
+
+            if (string.IsNullOrEmpty(Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride))
+            {
+                Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = "th-TH";
+            }
 
             InitializeComponent();
 
